cad130ce88221ffbc674ad833be9e6a3da51e9a3
prakritisubedii <prakriti.subedi@bison.howard.edu>
2026-09-01 13:15:42 -0400

    Add initial developer profile

---
+++ app.py
@@ -0,0 +1,4 @@
+print("Name: Prakriti Subedi")
+print("Major: Computer Science")
+print("Area of Technology: Machine learning")
+print("Skill to Develop: Deployment of models, experience with hardwares")
